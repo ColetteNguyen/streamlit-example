35fb1388423e3abed8b56ccb1cfa5617da92df94
Colette.Epiklah <109071567+ColetteNguyen@users.noreply.github.com>
2024-01-09 16:24:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,42 +12,35 @@ collection = dbname["roles"]
 """
 # Welcome to the Facility Management chatbot!
 """
-# Display input fields with labels indicating required and optional fields
-whatsapp_number = st.text_input('WhatsApp number* (Required)')
-mcst_number = st.text_input('MCST number* (Required)')
-resident_unit_number = st.text_input('Resident Unit number* (Required)')
-name = st.text_input('Name* (Required)')
-email = st.text_input('Email (Optional)')
-agree = st.checkbox('I agree to the PDPC policy')
+def create_form():
+    # Display input fields with labels indicating required and optional fields
+    whatsapp_number = st.text_input('WhatsApp number* (Required)')
+    mcst_number = st.text_input('MCST number* (Required)')
+    resident_unit_number = st.text_input('Resident Unit number* (Required)')
+    name = st.text_input('Name* (Required)')
+    email = st.text_input('Email (Optional)')
+    agree = st.checkbox('I agree to the PDPC policy')
+    
+    if st.button('Submit'):
+        if not whatsapp_number or not mcst_number or not resident_unit_number or not name:
+            st.warning('All fields are required.')
+        elif not agree:
+            st.warning('Please agree to the PDPC policy before submitting.')
+        else:
+            form_data = {
+                'role': 'resident',
+                'phone_number': whatsapp_number,
+                'mcst_no': mcst_number,
+                'resident_unit_number': resident_unit_number,
+                'name': name,
+                'email': email
+            }
+    
+            collection.insert_one(form_data)
+    
+            # Display a success message in a popup
+            st.success('Data submitted successfully!')
+create_form()
 
-if st.button('Submit'):
-    if not whatsapp_number or not mcst_number or not resident_unit_number or not name:
-        st.warning('All fields are required.')
-    elif not agree:
-        st.warning('Please agree to the PDPC policy before submitting.')
-    else:
-        form_data = {
-            'role': 'resident',
-            'phone_number': whatsapp_number,
-            'mcst_no': mcst_number,
-            'resident_unit_number': resident_unit_number,
-            'name': name,
-            'email': email
-        }
-
-        collection.insert_one(form_data)
-
-        # Display a success message in a popup
-        st.success('Data submitted successfully!')
-        st.write('Thank you')
         
-        # Reset form fields after successful submission
-        submitted = True
-        whatsapp_number = ""
-        mcst_number = ""
-        resident_unit_number = ""
-        name = ""
-        email = ""
-        agree = False
-
 
